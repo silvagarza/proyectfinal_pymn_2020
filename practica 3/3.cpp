5d6cdecacc88f77b33ef166aee1bbc1498904a41
silvagarza <75969396+silvagarza@users.noreply.github.com>
2020-12-16 19:16:00 -0800

    practica 3
    Esta carpeta contiene programas en c de la practica 3.

---
+++ practica 3/3.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+	int t1 = 0, t2 = 1, nt = 0, n, i;
+	printf("Secuencia Fibonacci \n);
+	printf("Ingresa el numero de terminos: \n);
+	scanf("%d", &n);
+	       
+	for (i = 1; i <= n; ++i){
+		printf("%d, ", t1);
+		nt = t1 + t2;
+		t1 = t2;
+		t2 = nt;
+	}
+	       
+	return 0;
+}
